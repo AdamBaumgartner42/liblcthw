2d6200a78d055b41de46ec269d60050598847c60
adambaumgartner42 <adambaumgartner@gmail.com>
2021-04-09 00:12:57 -0400

    Adding *array* bubble_sort()

--- src/lcthw/array_algos.h
+++ src/lcthw/array_algos.h
@@ -1,19 +1,10 @@
-#ifndef lcthw_Array_algos_h
-#define lcthw_Array_algos_h
-
+#ifndef lcthw_array_algos_h
+#define lcthw_array_algos_h
 
 typedef int (*compare_cb) (int a, int b);
 
-
+int sorted_order(int a, int b);
 int *Array_bubble_sort(int *numbers, int count, compare_cb cmp);
-
-
-int sorted_order(int a, int b)
-{
-    return a - b;
-}
-
-
-
+void print_array(int *array);
 
 #endif
--- tests/array_algos_tests.c
+++ tests/array_algos_tests.c
@@ -8,15 +8,48 @@
 int sorted_array[] = { 1, 2, 3, 4, 5, 6};
 int random_array[] = { 3, 7, 5, 8, 1, 4};
 
+int count = sizeof(sorted_array)/sizeof(int);
+
+int sorted_order(int a, int b)
+{
+    return a - b;
+}
+
+int reverse_order(int a, int b)
+{
+    return b-a;
+}
+    
 char *test_bubble_sort()
 {
-    //int count = sizeof(sorted_array)/sizeof(int);
-    //int *rc = Array_bubble_sort(random_array, count, (compare_cb) sorted_order);
-
+    int *rc = NULL; 
+    rc = random_array;
+    printf("Original array: ");
+    print_array(rc);
+    mu_assert(rc != NULL, "Error printing baseline array"); 
+    
+    rc = NULL; // reset
+    rc = Array_bubble_sort(random_array, count, sorted_order);
+    printf("Sorted array: ");
+    print_array(rc);    
+    mu_assert(rc != NULL, "Error with sorted_order"); 
+    
+    rc = NULL; // reset
+    rc = Array_bubble_sort(random_array, count, reverse_order);
+    printf("Reversed array: ");
+    print_array(rc);    
+    mu_assert(rc != NULL, "Error with reverse_order"); 
 
     return NULL;
 }
 
+void print_array(int *array)
+{
+    for(int i = 0; i < count; i++){
+        printf("%d, ", array[i]);
+    }
+    printf("\n");
+}
 
 char *all_tests()
 {
